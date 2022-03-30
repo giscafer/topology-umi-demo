e9e6705ea2655201018b76522372a14b8ff68a4d
giscafer <giscafer@outlook.com>
2022-03-30 14:55:59 +0800

    chore: add github url

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,15 +1,15 @@
-import { QuestionCircleOutlined } from "@ant-design/icons";
-import { Space } from "antd";
-import React from "react";
-import { useModel } from "umi";
-import ThemeSwitcher from "../ThemeSwitcher";
-import Avatar from "./AvatarDropdown";
-import styles from "./index.less";
+import { GithubOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
+import React from 'react';
+import { useModel } from 'umi';
+import ThemeSwitcher from '../ThemeSwitcher';
+import Avatar from './AvatarDropdown';
+import styles from './index.less';
 
-export type SiderTheme = "light" | "dark";
+export type SiderTheme = 'light' | 'dark';
 
 const GlobalHeaderRight: React.FC = () => {
-  const { initialState } = useModel("@@initialState");
+  const { initialState } = useModel('@@initialState');
 
   if (!initialState || !initialState.settings) {
     return null;
@@ -18,16 +18,24 @@ const GlobalHeaderRight: React.FC = () => {
   const { navTheme, layout } = initialState.settings;
   let className = styles.right;
 
-  if ((navTheme === "dark" && layout === "top") || layout === "mix") {
+  if ((navTheme === 'dark' && layout === 'top') || layout === 'mix') {
     className = `${styles.right}  ${styles.dark}`;
   }
   return (
     <Space className={className}>
+      <span
+        className={styles.action}
+        onClick={() =>
+          window.open('https://github.com/giscafer/topology-umi-demo')
+        }
+      >
+        <GithubOutlined />
+      </span>
       <ThemeSwitcher />
       <span
         className={styles.action}
         onClick={() => {
-          window.open("/~docs");
+          window.open('/~docs');
         }}
       >
         <QuestionCircleOutlined />
